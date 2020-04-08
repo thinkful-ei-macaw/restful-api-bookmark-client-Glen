fbb420842251e347633195612a3062163012f562
Alex Larrabee <nekotree556@gmail.com>
2020-04-07 20:29:36 -0700

    front end displays data, can't send data to back end yet

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// export default {
+//   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+//   API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+// }
+
+export default {
+  API_ENDPOINT: `http://localhost:8000/bookmarks`,
+  API_KEY: 'dummy-api-token'
+}
